87c0b86fbb9792b8bccb1c1c4db993739a424a68
Daniel Wagner <wagnerd58@gmail.com>
2019-10-20 10:40:13 -0600

    Add roxygen header to document fbind()

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,17 @@
+#' Bind two factors
+#'
+#' Create a new factor from two existing factors, where the new factors' levels
+#' are the union of the levels of the input factors.
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' fbind(iris$species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+#'
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
